66710013d34da80c220e3c130375efad2d1b3d64
Konrad <konradkrol110@gmail.com>
2025-10-24 18:50:07 +0200

    Suppress the logs in production

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,21 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
-  }
+  },
+  webpack(config, { dev }) {
+    const env = process.env.NODE_ENV;
+    const isProd = env === "production";
+    const isStaging = process.env.NEXT_PUBLIC_ENV === "staging";
+
+    // wycisz logi tylko w produkcji (nie w staging/dev)
+    if (isProd && !isStaging) {
+      ["log", "warn", "error", "info", "debug"].forEach(
+        (k) => ((console as any)[k] = () => { })
+      );
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
