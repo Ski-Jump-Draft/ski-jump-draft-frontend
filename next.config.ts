f36a01717710b3418508474aec43ad4e813c9d3f
Konrad <konradkrol110@gmail.com>
2025-10-24 19:59:23 +0200

    Finally remove the logs from production

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+const isStaging = process.env.NEXT_PUBLIC_ENV === "staging";
+
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // webpack(config, { dev }) {
-  //   const env = process.env.NODE_ENV;
-  //   const isProd = env === "production";
-  //   const isStaging = process.env.NEXT_PUBLIC_ENV === "staging";
-
-  //   // wycisz logi tylko w produkcji (nie w staging/dev)
-  //   if (isProd && !isStaging) {
-  //     ["log", "warn", "error", "info", "debug"].forEach(
-  //       (k) => ((console as any)[k] = () => { })
-  //     );
-  //   }
-
-  //   return config;
-  // },
+  compiler: {
+    removeConsole: isProd && !isStaging ? { exclude: ["error"] } : false,
+  },
 };
 
 export default nextConfig;
